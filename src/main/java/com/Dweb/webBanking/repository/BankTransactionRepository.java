b3d792493e9ff90076be6736182c7129685eacae
Cat Yeong <wjdxodud3@gmail.com>
2026-06-07 20:59:14 +0900

    DB 수정 커밋

--- src/main/java/com/Dweb/webBanking/repository/BankTransactionRepository.java
+++ src/main/java/com/Dweb/webBanking/repository/BankTransactionRepository.java
@@ -2,6 +2,8 @@ package com.Dweb.webBanking.repository;
 
 import com.Dweb.webBanking.domain.BankTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BankTransactionRepository extends JpaRepository<BankTransaction, Long> {
 }
